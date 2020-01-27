edb73b0f4a45063b68a86edc88c9433440a1040d
test111qqq <danlee0150@gmail.com>
2020-01-27 14:37:07 +0800

    Testing retrofit and add internet permission

--- app/src/main/java/com/drdlee/foodrecipe3/RecipeListActivity.java
+++ app/src/main/java/com/drdlee/foodrecipe3/RecipeListActivity.java
@@ -1,7 +1,22 @@
 package com.drdlee.foodrecipe3;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.drdlee.foodrecipe3.models.Recipe;
+import com.drdlee.foodrecipe3.request.RecipeApi;
+import com.drdlee.foodrecipe3.request.ServiceGenerator;
+import com.drdlee.foodrecipe3.responses.RecipeSearchResponse;
+import com.drdlee.foodrecipe3.util.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RecipeListActivity extends BaseActivity {
 
@@ -13,12 +28,44 @@ public class RecipeListActivity extends BaseActivity {
 		findViewById(R.id.test_button).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (mProgressBar.getVisibility() == View.VISIBLE) {
-					showProgressbar(false);
+				Toast.makeText(getApplicationContext(),"check logcat", Toast.LENGTH_LONG).show();
+				testRetrofitSearchRecipe();
+			}
+		});
+	}
+
+	private void testRetrofitSearchRecipe() {
+		RecipeApi recipeAPI = ServiceGenerator.getRecipeApi();
+
+		Call<RecipeSearchResponse> searchRecipes = recipeAPI.searchRecipe(
+				Constants.API_KEY, "chicken breast", "1"
+		);
+
+		searchRecipes.enqueue(new Callback<RecipeSearchResponse>() {
+			@Override
+			public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+				Log.d("RecipeListActivity", "onResponse: " + response.toString());
+				// ON LOGCAT: 2020-01-27 14:29:52.541 2916-2916/com.drdlee.foodrecipe3 D/RecipeListActivity: onResponse: Response{protocol=http/1.1, code=200, message=OK, url=https://recipesapi.herokuapp.com/api/search?key=&q=chicken%20breast&page=1}
+
+				if (response.code() == 200) {
+					Log.d("RecipeListActivity", "onResponse: body: " + response.body().toString());
+					// ON LOGCAT: 2020-01-27 14:29:52.543 2916-2916/com.drdlee.foodrecipe3 D/RecipeListActivity: onResponse: body: RecipeSearchResponse{count=6,
+					// recipes=[
+					// Recipe{title='Spinach Stuffed Chicken Breast', recipe_id='30372', image_url='https://res.cloudinary.com/dk4ocuiwa/image/upload/v1575163942/RecipesApi/988509965f.jpg', social_rank=99.9999, ingredients=null, publisher='All Recipes', source_url='http://allrecipes.com/Recipe/Spinach-Stuffed-Chicken-Breast/Detail.aspx'},
+					// ...]}
+
+					List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+					for (Recipe recipe : recipes) {
+						Log.d("RecipeListActivity", "onResponse: recipe title: " + recipe.getTitle());
+						// ON LOGCAT: 2020-01-27 14:29:52.543 2916-2916/com.drdlee.foodrecipe3 D/RecipeListActivity: onResponse: recipe title: Spinach Stuffed Chicken Breast
+						// and loop for all recipe title
+					}
 				}
-				else {
-					showProgressbar(true);
-				}
+			}
+
+			@Override
+			public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+
 			}
 		});
 	}
